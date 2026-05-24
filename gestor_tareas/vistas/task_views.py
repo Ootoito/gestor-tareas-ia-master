2161b3096991fb56efb9f3cbb45ff1c41b1661f3
Wladimir Cova <ootoito.totoy@gmail.com>
2026-05-24 12:13:50 +0100

    Corrige insercion de notas tras refactor de vistas

--- gestor_tareas/vistas/task_views.py
+++ gestor_tareas/vistas/task_views.py
@@ -83,8 +83,8 @@ def anadir_nota(request, tarea_id):
 def insertar_nota_tarea(id_tarea, usuario, texto):
     sql = """
         INSERT INTO gestor_tareas_tbnotas
-        (id_tarea, usuario, texto)
-        VALUES (%s, %s, %s)
+        (id_tarea, fecha, usuario, texto)
+        VALUES (%s, NOW(), %s, %s)
     """
 
     with connection.cursor() as cursor:
